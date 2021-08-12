96eebed5577b5ec936d32281a90dbf8222e48a4a
Chonthichar <chonthichar.soythong@gmail.com>
2021-08-12 23:05:45 +0700

    Fix formatting

--- main.js
+++ main.js
@@ -1,29 +1,26 @@
-
-function toFahrenheit(celsius)
-{
+function toFahrenheit(celsius) {
     let value = (celsius - 32) * 5 / 9
     return value.toFixed(2) + "C"
 }
 
-function toCelsius(fahrenheit)
-{
+function toCelsius(fahrenheit) {
     let value = (fahrenheit * 9) / 5 + 32
     return value.toFixed(2) + "F";
 }
-function display(elementLd, value)
-{
+
+function display(elementLd, value) {
     document.getElementById(elementLd).innerHTML = "<b>" + value + "</b>"
 }
 
-function toCelsiusProgram(value)
-{
+function toCelsiusProgram(value) {
     alert(toCelsius(value))
 }
-function toFahrenheitProgram(value){
+
+function toFahrenheitProgram(value) {
     alert(toFahrenheit(value))
 }
 
-const reset = () =>{
+const reset = () => {
     document.querySelector("#celsius").value = " ";
     document.querySelector("#fahrenheit").value = " ";
 }
